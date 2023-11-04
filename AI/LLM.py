9e786601d34b629c74f140b30ed27fa1c7e99bc6
c3041660 <c3041660@ncl.ac.uk>
2023-11-04 19:36:52 +0000

    Prompt Adjusting

--- AI/LLM.py
+++ AI/LLM.py
@@ -1,14 +1,13 @@
 from llama_cpp import Llama
-PROMPT = "Rewrite the lyrics to the following song in the most verboose and convoluted way imaginable. You must always refer in the first person as if you are the song. The lyrics must be in the language the song was originally written in. You must rewrite the entire song"
 
 class LLM:
 	def __init__(self):
-		self.__model = Llama("models/llama-2-7b.Q2_K.gguf", n_ctx=4096, last_n_tokens_size=4096)
+		self.__model = Llama("models/llama-2-7b.Q2_K.gguf", n_ctx=2**15, n_gqa=8)
 
 	def set_lyrics(self, lyrics):
 		self.__lyrics = lyrics
 
 	def generate(self):
 		if self.__model is not None:
-			response = self.__model(f"{PROMPT}: {self.__lyrics}")
+			response = self.__model(f"Compose a humorous first-person narrative as if you are the following lyrics: [{self.__lyrics}] providing a whimsical tale of the song's content, including a comical exploration of its verses, pre-chorus, chorus, and any humorous elements within the lyrics. Ensure to keep the language the same as the original lyrics")
 			return response["choices"][0]["text"]
