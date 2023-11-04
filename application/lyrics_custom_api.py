b37879faacc269cb9ff0f663168fdce9fa7f4797
George Tsakoumakis <57140924+GeorgeTsakoumakis@users.noreply.github.com>
2023-11-04 23:28:22 +0200

    Complete full stack boilerplate functionality

---
+++ application/lyrics_custom_api.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+import requests
+import urllib.parse
+import xml.etree.ElementTree as ET
+
+
+def get_lyrics(artist: str, song: str) -> Optional[str]:
+    r = requests.get(
+        f"http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist={urllib.parse.quote_plus(artist)}&song={urllib.parse.quote_plus(song)}")
+    root = ET.fromstring(r.text)
+
+    # Iterate through each "SearchLyricResult" element
+    for search_lyric_result in root:
+        lyric_id = None
+        lyric_checksum = None
+        for x in search_lyric_result:
+            if x.tag.endswith('LyricId'):
+                lyric_id = x.text
+            elif x.tag.endswith('LyricChecksum'):
+                lyric_checksum = x.text
+        if lyric_id is None or lyric_checksum is None:
+            continue
+        else:
+            r = requests.get(
+                f"http://api.chartlyrics.com/apiv1.asmx/GetLyric?lyricId={urllib.parse.quote_plus(lyric_id)}&lyricChecksum={urllib.parse.quote_plus(lyric_checksum)}")
+            root2 = ET.fromstring(r.text)
+            for n in root2:
+                if n.tag.endswith("}Lyric"):
+                    return n.text
+    return None
+
+
+if __name__ == '__main__':
+    print(get_lyrics('Gorillaz', 'Feel Good Inc.'))
