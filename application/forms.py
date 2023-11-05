afba8cc674a73ef0df84ae4f01fb41d8794154f6
tinyTim567 <tmck911@gmail.com>
2023-11-05 02:23:30 +0000

    Update text on index page

--- application/forms.py
+++ application/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
     title = StringField(validators=[DataRequired()])
     artist = StringField(validators=[DataRequired()])
-    submit = SubmitField()
+    submit = SubmitField(name="Ruin Eloquently")
